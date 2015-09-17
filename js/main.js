79bceea1636c57ed1e16b78594a7485ceca86d40
bryantee <bryantee@gmail.com>
2015-09-17 15:52:01 -0700

    Testing strava api basics

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
 	$('.heading, .tagline, .social').delay(500).css('display', 'visibility').hide().fadeIn('slow');
 });
+
+function getStrava() {
+
+	var params = {
+		client_id: 5556,
+		response_type: 'code',
+		redirect_uri: 'http://bryanswagerty.com'
+	};
+	
+	url = 'https://www.strava.com/oauth/authorize';
+
+	$.getJSON(url, params, function(data) {
+		console.log('success');
+	});
+
+}
