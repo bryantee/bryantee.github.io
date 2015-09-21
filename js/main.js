0d710b106e8cf7f2b929b5a2fd67de1281d8e3f7
bryantee <bryantee@gmail.com>
2015-09-21 15:48:24 -0700

    Strava API ajax request added. Basic render to page.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,45 @@
 $(document).ready(function() {
-	$('.heading, .tagline, .social').delay(500).css('display', 'visibility').hide().fadeIn('slow');
+	$('.heading, .tagline, .social').delay(500).css('display', 'visibility').hide().delay(100).fadeIn('slow');
+
+	// Make call to strava and set YTD info as variable
+	getStrava();
+
 });
 
-function getStrava() {
+function appendStrava(miles, elevation) {
+	miles = Number(miles).toLocaleString('en');
+	elevation = Number(elevation).toLocaleString('en');
 
-	var params = {
-		client_id: 5556,
-		response_type: 'code',
-		redirect_uri: 'http://bryanswagerty.com'
+	$('#miles').append(miles + ' mi')
+	$('#elevation').append(elevation + ' ft');
+
+	// countUp.js attempt
+
+/*	var options = {
+		useEasing: true,
+		seperator: ',',
+		prefix: '',
+		suffix: ''
 	};
-	
-	url = 'https://www.strava.com/oauth/authorize';
 
-	$.getJSON(url, params, function(data) {
-		console.log('success');
+	var stravaMiles = new CountUp("#miles", 0, 20, 0, 4);
+	var stravalElevation = new CountUp("#elevation", 0, 15, 0, 4);
+
+	stravaMiles.start();
+	stravalElevation.start();	*/
+
+
+
+}
+
+function getStrava() {
+	$.getJSON('https://www.strava.com/api/v3/athletes/3076072/stats?access_token=5c9386df1ea0ae7be4002f15222998e4005dfa24&callback=?', function(data) {
+		console.log('successful Strava API call');
+		rideTotals = data.ytd_ride_totals;
+		totalMiles = parseInt(rideTotals.distance * 0.000621371);
+	 	totalElevation = parseInt(rideTotals.elevation_gain * 3.28084);
+	 	//append to dom, call function
+	 	appendStrava(totalMiles, totalElevation);
 	});
 
 }
